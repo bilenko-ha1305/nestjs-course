3c3ed57e7949edff280f793bc6a09071c03e8b11
Hryhoriy Bilenko <hryhoriy.bilenko@dbbsoftware.com>
2024-08-16 13:39:24 +0300

    add statuses

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll() {
-    return 'This action returns all coffees';
+  findAll(@Res() response) {
+    response.status(200).send('This action returns all coffees');
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `This action returns #${id} coffee`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.GONE)
+  create(@Body() body) {
+    return body;
   }
 }
